2af2d5c1ec568a2d5e7a4828f94e6316ed6e1840
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-13 08:39:52 -0300

    Exercício 14 lista 3

--- Listas/Lista2/Ex14.c
+++ Listas/Lista2/Ex14.c
@@ -7,6 +7,30 @@ E = 1/N + 1/(N-1) + 1/(N-2) + ... + 1
 
 #include<stdio.h>
 
+float calcula_E(int N) {
+    
+    float resultado = 0, termo;
+
+    for(int i=0; i<N; i++) {
+        termo = 1.0/(N-i);
+        resultado += termo;
+    }
+
+    return resultado;
+}
+
 int main() {
+
+    int N;
+
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &N);
+
+    if(N<=0) {
+        printf("A expressão não pode ser calculada");
+    }
+    else {
+        printf("E = %f", calcula_E(N));
+    }
     return 0;
 }
