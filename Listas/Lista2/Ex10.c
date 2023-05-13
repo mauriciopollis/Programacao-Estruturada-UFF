0032078f09d82c22b42eae70e2f801c9a27a60b1
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-13 07:57:08 -0300

    Exercício 10 lista3

--- Listas/Lista2/Ex10.c
+++ Listas/Lista2/Ex10.c
@@ -11,5 +11,46 @@ Obs: O final da leitura de dados é marcado por uma idade negativa.
 #include<stdio.h>
 
 int main() {
+
+    int idade, contador_homens = 0, contador_mulheres = 0;
+    char sexo;
+    float salario, media_salarial_homens = 0, media_salarial_mulheres = 0, maior_salario_abaixo_30 = 0;
+
+    printf("Idade: ");
+    scanf("%d", &idade);
+
+    while(idade>=0) {
+        printf("Sexo (m ou f): ");
+        scanf(" %c", &sexo);
+        printf("Salário: ");
+        scanf("%f", &salario);
+        if(sexo == 'm') {
+            contador_homens += 1;
+            media_salarial_homens += salario;
+        }
+        else {
+            if(sexo == 'f') {
+                contador_mulheres += 1;
+                media_salarial_mulheres += salario;
+            }
+        }
+
+        if(idade<30) {
+            if(salario>maior_salario_abaixo_30) {
+                maior_salario_abaixo_30 = salario;
+            }
+        }
+
+        printf("Idade: ");
+        scanf("%d", &idade);
+    }
+
+    media_salarial_homens = media_salarial_homens/contador_homens;
+    media_salarial_mulheres = media_salarial_mulheres/contador_mulheres;
+
+    printf("Média salarial dos homens: %f\n", media_salarial_homens);
+    printf("Média salarial das mulheres: %f\n", media_salarial_mulheres);
+    printf("Maior salário abaixo de 30 anos: %f", maior_salario_abaixo_30);
+
     return 0;
 }
