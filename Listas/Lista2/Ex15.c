9b7a4d3dd8978e9bdc5b415d1206965809ffc492
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-13 08:45:55 -0300

    Exercício 15 lista 3

--- Listas/Lista2/Ex15.c
+++ Listas/Lista2/Ex15.c
@@ -4,6 +4,25 @@ Faça um programa que calcule o fatorial de um número N.
 
 #include<stdio.h>
 
+int fatorial(int N) {
+
+    int fatorial = 1;
+
+    for(int i=1; i<=N; i++) {
+        fatorial *= i;
+    }
+
+    return fatorial;
+}
+
 int main() {
+
+    int N;
+
+    printf("Digite um número inteiro não negativo: ");
+    scanf("%d", &N);
+
+    printf("%d! = %d", N, fatorial(N));
+
     return 0;
 }
