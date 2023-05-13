8aa4369a183a9509dadd2c83e931773e103c416c
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-13 08:49:49 -0300

    Exercício 16 lista 3

--- Listas/Lista2/Ex16.c
+++ Listas/Lista2/Ex16.c
@@ -6,5 +6,18 @@ E = 2^1 + 2^2 + 2^3 + ... + 2^(N-1) + 2^N
 #include<stdio.h>
 
 int main() {
+
+    int N, E = 0, termo = 2;
+
+    printf("Digite N: ");
+    scanf("%d", &N);
+
+    for(int i=1; i<=N; i++) {
+        E += termo;
+        termo *= 2;
+    }
+
+    printf("E = %d", E);
+    
     return 0;
 }
