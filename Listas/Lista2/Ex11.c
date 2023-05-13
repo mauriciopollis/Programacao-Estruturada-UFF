c44c4206f0af349ea85291d7379557e26cc16977
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-13 08:01:40 -0300

    Exercício 11 lista 3

--- Listas/Lista2/Ex11.c
+++ Listas/Lista2/Ex11.c
@@ -5,5 +5,22 @@ Faça um programa que escreva os N primeiros termos de uma PA. O primeiro termo,
 #include<stdio.h>
 
 int main() {
+
+    int a1, razao, a, N;
+
+    printf("Digite N: ");
+    scanf("%d", &N);
+    printf("Primeiro termo: ");
+    scanf("%d", &a1);
+    printf("Razão da P.A: ");
+    scanf("%d", &razao);
+
+    a = a1;
+
+    for(int i=1; i<=N; i++) {
+        printf("%d ", a);
+        a += razao;
+    }
+
     return 0;
 }
