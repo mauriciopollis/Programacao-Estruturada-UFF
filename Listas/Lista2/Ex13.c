01ad1684de725b7211eea67df4bdb2efdb03cdc3
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-13 08:28:33 -0300

    Exercício 13 lista 3

--- Listas/Lista2/Ex13.c
+++ Listas/Lista2/Ex13.c
@@ -4,7 +4,30 @@ Faça um programa que leia os nomes e os preços dos produtos de uma loja e que 
 */
 
 #include<stdio.h>
+#include<string.h>
 
 int main() {
+
+    char codigo[4], codigo_mais_caro[4];
+    float preco, maior_preco = 0;
+
+    printf("Digite o código do produto: ");
+    scanf(" %s", codigo);
+
+    while(strcmp(codigo, "XXX") != 0) {
+        
+        printf("Preço: ");
+        scanf("%f", &preco);
+        
+        if(preco > maior_preco) {
+            strcpy(codigo_mais_caro, codigo);
+        }
+
+        printf("Digite o código do produto: ");
+        scanf(" %s", codigo);
+    }
+
+    printf("Código mais caro: %s", codigo_mais_caro);
+
     return 0;
 }
