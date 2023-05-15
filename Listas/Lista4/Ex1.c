d8600196c5eb31556d7a925864bd462691aca791
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-15 12:48:20 -0300

    Exercícios 1 e 2 da lista de struct

--- Listas/Lista4/Ex1.c
+++ Listas/Lista4/Ex1.c
@@ -7,7 +7,47 @@ que leia os dados de 20 pessoas, e que calcule e mostre:
 */
 
 #include<stdio.h>
+#define NUM_HABITANTES 3
+
+struct habitante {
+    
+    float salario;
+    int idade, numero_de_filhos;
+};
+typedef struct habitante Habitante;
 
 int main() {
+
+    Habitante vetor_habitantes[NUM_HABITANTES];
+    float media_salarial, media_numero_filhos, maior_salario = 0;
+
+    for(int i=1; i<=NUM_HABITANTES; i++) {
+
+        printf("Digite o salário do habitante %d: ", i);
+        scanf("%f", &vetor_habitantes[i-1].salario);
+        printf("Digite a idade do habitante %d: ", i);
+        scanf("%d", &vetor_habitantes[i-1].idade);
+        printf("Digite o número de filhos do habitante %d: ", i);
+        scanf("%d", &vetor_habitantes[i-1].numero_de_filhos);
+
+    }
+
+    for(int i=0; i<NUM_HABITANTES; i++) {
+
+        media_salarial += vetor_habitantes[i].salario;
+        media_numero_filhos += vetor_habitantes[i].numero_de_filhos;
+        if(vetor_habitantes[i].salario>maior_salario) {
+            maior_salario = vetor_habitantes[i].salario;
+        }
+
+    }
+
+    media_salarial = media_salarial/NUM_HABITANTES;
+    media_numero_filhos = media_numero_filhos/NUM_HABITANTES;
+
+    printf("A média salarial da população é: %f\n", media_salarial);
+    printf("A média do número de filhos da população é: %f\n", media_numero_filhos);
+    printf("O maior salário da população é: %f", maior_salario);
+
     return 0;
 }
--- Listas/Lista4/Ex2.c
+++ Listas/Lista4/Ex2.c
@@ -7,7 +7,50 @@ estado, o número de veículos, e a quantidade de acidentes. Faça um programa q
 */
 
 #include<stdio.h>
+#include<string.h>
+
+#define NUM_ESTADOS 20
+
+struct estado {
+
+    char sigla[3];
+    int numero_veiculos, quantidade_acidentes;
+
+};
+typedef struct estado Estado;
 
 int main() {
+
+    Estado vetor_estados[NUM_ESTADOS];
+    float menor_indice_acidentes;
+    int total_acidentes = 0, maior_numero_carros = 0;
+    char sigla_mais_carros[3], sigla_menor_indice;
+
+    for(int i=0; i<NUM_ESTADOS; i++) {
+        printf('Digite a sigla do estado: ');
+        scanf(" %s", vetor_estados[i].sigla);
+        prtinf("Digite o número de veículos desse estado: ");
+        scanf("%d", &vetor_estados[i].numero_veiculos);
+        printf("Digite o número de acidentes desse estado: ");
+        scanf("%d", &vetor_estados[i].quantidade_acidentes);
+    }
+
+    menor_indice_acidentes = (float)vetor_estados[0].quantidade_acidentes/vetor_estados[0].numero_veiculos;
+
+    for(int i=0; i<NUM_ESTADOS; i++) {
+        if(vetor_estados[i].numero_veiculos>maior_numero_carros) {
+            strcpy(sigla_mais_carros, vetor_estados[i].sigla);
+        }
+        float indice_acidentes = (float)vetor_estados[i].quantidade_acidentes/vetor_estados[i].numero_veiculos;
+        if(indice_acidentes<menor_indice_acidentes) {
+            strcpy(sigla_menor_indice, vetor_estados[i].sigla);
+        }
+        total_acidentes += vetor_estados[i].quantidade_acidentes;
+    }
+
+    printf("O estado com maior número de carros é: %s", sigla_mais_carros);
+    printf("O estado com o menor índice de acidentes é: %s", sigla_menor_indice);
+    printf("O número total de acidentes é: %d", total_acidentes);
+
     return 0;
 }
