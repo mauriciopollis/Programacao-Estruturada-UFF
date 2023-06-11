94828f1f1b52690e2cd059b959b72caa7b8acff9
Mauricio Pollis <mauriciopollis@gmail.com>
2023-06-11 17:03:16 -0300

    Encapsulamento da definição da estrutura, fazendo sua declaração no arquivo .c e não no arquivo .h

--- Livro/Parte-2/Problemas-livres/Cap9/Modularizacao/main.c
+++ Livro/Parte-2/Problemas-livres/Cap9/Modularizacao/main.c
@@ -3,15 +3,19 @@
 
 int main(void) {
 
-    Ponto2D ponto1, ponto2;
-    float dist, dist_origem1, dist_origem2;
+    Ponto2D *ponto1, *ponto2;
+
+    float dist, dist_origem1, dist_origem2, x, y;
     char sair;
 
     printf("Coordenadas do primeiro ponto: ");
-    scanf("%f %f", &ponto1.x, &ponto1.y);
-    printf("Coordenadas do segundo ponto: ");
-    scanf("%f %f", &ponto2.x, &ponto2.y);
+    scanf("%f %f", &x, &y);
+    ponto1 = cria_ponto(x, y);
 
+    printf("Coordenadas do segundo ponto: ");
+    scanf("%f %f", &x, &y);
+    ponto2 = cria_ponto(x, y);
+    
     dist = distancia_entre_pontos_2d(ponto1, ponto2);
     dist_origem1 = distancia_ate_origem(ponto1);
     dist_origem2 = distancia_ate_origem(ponto2);
--- Livro/Parte-2/Problemas-livres/Cap9/Modularizacao/ponto.c
+++ Livro/Parte-2/Problemas-livres/Cap9/Modularizacao/ponto.c
@@ -1,15 +1,28 @@
 #include<math.h>
+#include<stdlib.h>
 #include "ponto.h"
 
-float distancia_entre_pontos_2d(Ponto2D p1, Ponto2D p2) {
+struct ponto2d {
+    float x;
+    float y;
+};
+
+float distancia_entre_pontos_2d(Ponto2D *p1, Ponto2D *p2) {
     float distancia;
-    distancia = sqrt(pow((p1.x - p2.x), 2) + pow((p1.y- p2.y), 2));
+    distancia = sqrt(pow((p1->x - p2->x), 2) + pow((p1->y - p2->y), 2));
     return distancia;
 }
 
-float distancia_ate_origem(Ponto2D p) {
+float distancia_ate_origem(Ponto2D *p) {
     float distancia;
 
-    distancia = sqrt(pow(p.x, 2) + pow(p.y, 2));
+    distancia = sqrt(pow(p->x, 2) + pow(p->y, 2));
     return distancia;
+}
+
+Ponto2D* cria_ponto(float x, float y) {
+    Ponto2D *p = (Ponto2D *) malloc(sizeof(Ponto2D));
+    p->x = x;
+    p->y = y;
+    return p;
 }
